f0a055ef2e0542faf2ad2207a959294d60f816ff
Fabian Carlström <inrick@gmail.com>
2015-11-08 22:19:32 +0100

    Add helpers for Option

--- util.ml
+++ util.ml
@@ -5,3 +5,19 @@ let range i j =
     | k when k < i -> ns
     | k -> go (k :: ns) (k-1) in
   go [] j
+
+module Option = struct
+  type 'a t = 'a option
+
+  let map f = function
+    | Some x -> Some (f x)
+    | None -> None
+
+  let iter f = function
+    | Some x -> f x
+    | None -> ()
+
+  let maybe default f = function
+    | Some x -> f x
+    | None -> default
+end
