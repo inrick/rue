db36550502e9a882204561daadff6b8b2924fb0b
Fabian Carlström <inrick@gmail.com>
2016-09-29 23:03:54 +0200

    Make help function for checking if exception was raised

--- src/test.ml
+++ src/test.ml
@@ -2,6 +2,12 @@ open Util
 
 let eval = Repl.(parse_string >> eval)
 
+let assert_raise exn expr =
+  assert (match expr () with
+  | exception x when x = exn -> true
+  | exception _ -> false
+  | _ -> false)
+
 let () =
   assert (eval "1+1 2 3" = "2 3 4");
   assert (eval "!5" = "0 1 2 3 4");
@@ -9,13 +15,5 @@ let () =
   assert (eval "(1+!10)*|1+!10" = "10 18 24 28 30 30 28 24 18 10");
   assert (eval "900.%5*(1+!10)*|1+!10" =
     "18. 10. 7.5 6.42857142857 6. 6. 6.42857142857 7.5 10. 18.");
-  assert (
-    match eval "1 2 3+!4" with
-    | exception Eval.Dim_error -> true
-    | _ -> false
-  );
-  assert (
-    match eval "!7." with
-    | exception Eval.Type_error -> true
-    | _ -> false
-  );
+  assert_raise Eval.Dim_error (fun () -> eval "1 2 3+!4");
+  assert_raise Eval.Type_error (fun () -> eval "!7.");
